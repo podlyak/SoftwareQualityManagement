d237b8b6b756316af9e7ca47e4e9b3e086840cdb
podlyak <viktorpolyakov1999@mail.ru>
2021-05-26 05:29:27 +0300

    Added REST API unit tests, close #5

---
+++ QualityManagement.Tests/RestAPITests.cs
@@ -0,0 +1,19 @@
+﻿using Xunit;
+using QualityManagement.API.Controllers;
+using QualityManagement.Shared;
+
+namespace QualityManagement.Tests
+{
+    public class RestAPITests
+    {
+        [Theory]
+        [InlineData("a a a b b b a a a b b b a a a")]
+        public async void TestTextIndex(string request)
+        {
+            var controller = new PieceOfTextController();
+            string result = await controller.Get(request);
+            var data = new BasicEnvironment();
+            Assert.Equal(data.TestData1.Results.ToSimpleText(), result);
+        }
+    }
+}
