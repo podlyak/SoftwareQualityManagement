c3e79cb8a027ad2ccb130a4dd0a985b470dd86e6
podlyak <viktorpolyakov1999@mail.ru>
2021-05-26 05:14:44 +0300

    Added code for converting a dictionary to a string, close #2

---
+++ QualityManagment.Shared/Extensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QualityManagement.Shared
+{
+    public static class Extensions
+    {
+        public static string ToSimpleText<K, V>(this IDictionary<K, V> dict)
+        {
+            var res = new StringBuilder();
+            foreach (var pair in dict)
+            {
+                res.AppendLine($"{pair.Key};{pair.Value}");
+            }
+
+            return res.ToString();
+        }
+    }
+}
