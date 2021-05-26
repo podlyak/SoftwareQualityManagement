e0fb66843d251107b87793034dce85e0ba664912
podlyak <viktorpolyakov1999@mail.ru>
2021-05-26 05:27:13 +0300

    Added code of the REST API, close #4

---
+++ QualityManagement.API/Controllers/PieceOfTextController.cs
@@ -0,0 +1,16 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using QualityManagement.Shared;
+using static QualityManagement.Shared.PieceOfText;
+
+namespace QualityManagement.API.Controllers {
+    [ApiController]
+    [Route("[controller]")]
+    public class PieceOfTextController : ControllerBase {
+        
+        [HttpGet("{text}")]
+        public async Task<string> Get(string text) {
+            return await Task.Run(() => SearchForFragments(text).ToSimpleText());
+        }
+    }
+}
